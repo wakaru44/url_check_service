518b9177d5bac7e217133f393d4ff96973858600
Juan Morales <juan.morales@picsolve.com>
2015-10-12 18:46:33 +0100

    We are Now AJAX :). Only fixing the test validator undergoing

--- static/checker.js
+++ static/checker.js
@@ -1,70 +1,49 @@
 
 // My functions to check remote URLs
 
-function httpGet(theUrl)
-{
-	var xmlHttp = null;
-	xmlHttp = new XMLHttpRequest();
-	xmlHttp.open("GET", theUrl, false);
-	xmlHttp.send( null );
-	return xmlHttp.status.toString()
-	//return { //alt return with extra data
-	//	text: xmlHttp.responseText,
-	//	status_code: xmlHttp.status_code
-	//};
-}
-
 function checkRemoteUrlOnLocal( url )
 {
 	// We will have to transform that url into a valid internal query to our backend
 	base_url = get_base_url();
 	checker_url = base_url + "/check?url=" + encodeURIComponent(url);
-	//result =httpGet( checker_url ); //old, sync crap
 	//TODO: This crap is not working
 	var status_und_stuff;
 	result = $.ajax( checker_url )
-	//result.success( function(response_data, statusText, xhrObj)
-			//{
-				//console.log("then whatever");
-				//console.log(response_data);
-				//console.log(statusText);
-				//console.log(xhrObj);
-				//status_und_stuff = xhrObj.status;
-				//return status_und_stuff;
-			//})
-		//.fail( function(a) { console.log("shit happens", a); } );
-	//result.fail(function(data){console.log("failed"); console.log(data.status.toString()); status_und_stuff = data.status})
-	//result.done(function(data){console.log("success"); console.log(200); status_und_stuff = 200})
 
-	//console.log("this result should be a 200 or a 500 ")
-	//console.log("status_und_stuff")
-	//console.log(status_und_stuff)
-	//console.log("reslut")
-	//console.log(result)
 	return result
 }
 
-function verify_url( spec, url )
-{ // check that the url passes the test
-	return checkRemoteUrlOnLocal(url); //check contais the .text and .status_code
-//	code = check
-//	if (check == spec)
-//	{
-//		return "Ok" //KISS
-//	}
-//	else
-//	{
-//		return "The result doesn't match Spec;\n " + "check: " + check + "spec: " + spec
-//	}
-}
 
 function get_base_url()
 {
 	var http = location.protocol;
 	var slashes = http.concat("//");
-	//var host = slashes.concat(window.location.hostname);
 	var host = slashes.concat(window.location.host); //host not hostname to include port if is not 80
 	return host
 }
 
 
+function pass_the_test( spec, responseObject, obj)
+{
+	if (responseObject.status == spec)
+	{ //If the test failed, do magic and paint button red
+		obj.style.backgroundColor = "green";
+		obj.style.color = "white";
+
+		console.log("We have succeded the test");
+		console.log(responseObject);
+
+		result_display = "Ok"
+	} else {
+		//if passes, paint it green
+		obj.style.backgroundColor = "red";
+		obj.style.color = "white";
+
+		console.log("We have failed the test?")
+		console.log(responseObject)
+		console.log(obj)
+		result_display = $(obj).attr("href") + " test failed: \n Expected: " + spec + "; Got: " + responseObject.status;
+	}
+
+	return result_display;
+}
